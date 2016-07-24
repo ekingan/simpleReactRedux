ff8356d8a1d1c6477fc91ae7c404d439682b5fc2
ekingan <ekingan@gmail.com>
2016-07-24 09:23:04 -0700

    first commit

---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'Javascript: the good parts', pages: 101},
+		{ title: 'Eloquent Ruby', pages: 200 },
+		{ title: 'Even Cowgirls Get the Blues', pages: 300 },
+		{ title: 'Goodhouse', pages: 311 }
+	]
+}
